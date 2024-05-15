a7f848a4e82fcafb79bceeb659a8d95f5d00f633
TranTuanMinh-rmit <s3804812@rmit.edu.vn>
2024-05-15 23:55:11 +0700

    Finish Dependent View to spec, refined their Claim View.

--- src/main/java/main_folder/Controller/dependentController.java
+++ src/main/java/main_folder/Controller/dependentController.java
@@ -13,6 +13,7 @@ import javafx.stage.Stage;
 import main_folder.ConnectDatabase.database;
 import main_folder.Model.Dependent;
 import main_folder.Model.Claim;
+import main_folder.Model.PolicyHolder;
 
 import java.io.IOException;
 import java.net.URL;
@@ -25,17 +26,12 @@ public class dependentController extends Thread implements Initializable {
     private Button logoutBTN;
 
     //Stuff for Dependent View
-    @FXML
-    private TableView<Dependent> infoTable;
     private ObservableList<Dependent> dependentData = FXCollections.observableArrayList();
+    private ObservableList<PolicyHolder> holderData = FXCollections.observableArrayList();
     @FXML
-    private TableColumn<Dependent, String> dependentEmailColumn;
+    private TextArea dependentInfo;
     @FXML
-    private TableColumn<Dependent, String> dependentIdColumn;
-    @FXML
-    private TableColumn<Dependent, String> dependentNameColumn;
-    @FXML
-    private TableColumn<Dependent, String> dependentPolicyNumberColumn;
+    private TextArea dependentHolder;
 
     //Stuff for Claim View
     @FXML
@@ -59,6 +55,7 @@ public class dependentController extends Thread implements Initializable {
     private TableColumn<Claim, String> receiverBankingInfoColumn;
 
 
+    //Will be using something else instead of TableView.
     public void RetrieveDependent() {
         database db = new database();
         try (Connection conn = db.connect()){
@@ -68,6 +65,7 @@ public class dependentController extends Thread implements Initializable {
                     int dependentId = Integer.parseInt(loginController.getLoggedInUser());
                     String sql = "SELECT * FROM \"User\" WHERE \"id\" = ?";
                     String sql2 = "SELECT * FROM \"Dependent\" WHERE \"userID\" = ?";
+                    String sql3 = "SELECT * FROM \"Customer\" WHERE \"id\" = ?";
                     PreparedStatement stmt = conn.prepareStatement(sql);
                     stmt.setInt(1, dependentId);
                     ResultSet rs = stmt.executeQuery();
@@ -82,17 +80,26 @@ public class dependentController extends Thread implements Initializable {
                         PreparedStatement stmt2 = conn.prepareStatement(sql2);
                         stmt2.setInt(1, dependentId);
                         ResultSet rs2 = stmt2.executeQuery();
-                        if (rs2.next()) {
-                            Dependent dependent = new Dependent(
-                                    rs.getString("id"),
-                                    rs.getString("name"),
-                                    rs.getString("email"),
-                                    rs2.getString("policyNumber")
-                            );
-                            System.out.println("Dependent object created: " + dependent); // Debug line
-                            dependentData.add(dependent);
-                            System.out.println("Dependent object added to ObservableList."); // Debug line
-                            System.out.println("Size of dependentData: " + dependentData.size()); // Debug line
+                        if (!rs2.next()) {
+                            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                            alert.setTitle("Retrieve Dependent notification");
+                            alert.setHeaderText(null);
+                            alert.setContentText("The Dependent corresponds to this id cannot be found");
+                            alert.showAndWait();
+                        }else {
+                            int customerId = rs2.getInt("policyNumber");
+                            PreparedStatement stmt3 = conn.prepareStatement(sql3);
+                            stmt3.setInt(1, customerId);
+                            ResultSet rs3 = stmt3.executeQuery();
+                            if (rs3.next()) {
+                                Dependent dependent = new Dependent(
+                                        rs.getString("id"),
+                                        rs.getString("name"),
+                                        rs.getString("email"),
+                                        rs3.getString("InsuranceNumber")
+                                );
+                                dependentData.add(dependent);
+                            }
                         }
                     }
                 } catch (NumberFormatException e) {
@@ -108,12 +115,93 @@ public class dependentController extends Thread implements Initializable {
         catch( SQLException e){
             e.printStackTrace();
         }
-
-        infoTable.setItems(dependentData);
-        System.out.println("TableView updated with ObservableList."); // Debug line
+        for (Dependent dependent : dependentData) {
+            dependentInfo.appendText(dependent.toString() + "\n");
+        }
     }
 
-    //The base code that I reference is meant to insert an ID then a new window pops one out. This will not work.
+    public void RetrieveHolder() {
+        database db = new database();
+        try (Connection conn = db.connect()) {
+            if (conn != null) {
+                System.out.println("Database connection successful."); // Debug line
+                try {
+                    int dependentId = Integer.parseInt(loginController.getLoggedInUser());
+                    String sql1 = "SELECT * FROM \"Dependent\" WHERE \"userID\" = ?";
+                    String sql2 = "SELECT * FROM \"PolicyHolder_Dependent\" WHERE \"Dependent\" = ?";
+                    String sql3 = "SELECT * FROM \"PolicyHolder\" WHERE \"id\" = ?";
+                    String sql4 = "SELECT * FROM \"User\" WHERE \"id\" = ?";
+                    PreparedStatement stmt1 = conn.prepareStatement(sql1);
+                    stmt1.setInt(1, dependentId);
+                    ResultSet rs1 = stmt1.executeQuery();
+                    if (!rs1.next()){
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Retrieve Dependent notification");
+                        alert.setHeaderText(null);
+                        alert.setContentText("The Dependent corresponds to this id cannot be found");
+                        alert.showAndWait();
+                    }
+                    else {
+                        int relationship = rs1.getInt("id");
+                        PreparedStatement stmt2 = conn.prepareStatement(sql2);
+                        stmt2.setInt(1, relationship);
+                        ResultSet rs2 = stmt2.executeQuery();
+                        if (!rs2.next()){
+                            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                            alert.setTitle("Retrieve Holder notification");
+                            alert.setHeaderText(null);
+                            alert.setContentText("The Holder corresponds to this id cannot be found");
+                            alert.showAndWait();
+                        }
+                        else {
+                            int holderId = rs2.getInt("PolicyHolder");
+                            PreparedStatement stmt3 = conn.prepareStatement(sql3);
+                            stmt3.setInt(1, holderId);
+                            ResultSet rs3 = stmt3.executeQuery();
+                            if (!rs3.next()){
+                                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                                alert.setTitle("Retrieve Holder notification");
+                                alert.setHeaderText(null);
+                                alert.setContentText("The Holder corresponds to this id cannot be found");
+                                alert.showAndWait();
+                            }
+                            else {
+                                int userId = rs3.getInt("userID");
+                                PreparedStatement stmt4 = conn.prepareStatement(sql4);
+                                stmt4.setInt(1, userId);
+                                ResultSet rs4 = stmt4.executeQuery();
+                                if (rs4.next()){
+                                    PolicyHolder holder = new PolicyHolder(
+                                            rs4.getString("id"),
+                                            rs4.getString("name"),
+                                            rs4.getString("email"),
+                                            rs3.getString("policyNumber")
+                                    );
+                                    holderData.add(holder);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (NumberFormatException e) {
+                    // handle error
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Retrieve Customer notification");
+                    alert.setHeaderText(null);
+                    alert.setContentText("The Customer corresponds to this id does not exist");
+                    alert.showAndWait();
+                }
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        for (PolicyHolder holder : holderData) {
+            dependentHolder.appendText(holder.printForDependent() + "\n");
+        }
+    }
+
+    // Retrieve the claim data from the database. Can shorten the queries, there's a redundant one.
     public void RetrieveClaim() {
         database db = new database();
         try (Connection conn = db.connect()){
@@ -122,9 +210,7 @@ public class dependentController extends Thread implements Initializable {
                 try {
                     int dependentId = Integer.parseInt(loginController.getLoggedInUser());
                     String sql = "SELECT * FROM \"Dependent\" WHERE \"userID\" = ?";
-                    String sql2 = "SELECT * FROM \"Customer\" WHERE \"id\" = ?";
                     String sql3 = "SELECT * FROM \"Claim\" WHERE \"Insured_Person\" = ?";
-
                     PreparedStatement stmt = conn.prepareStatement(sql);
                     stmt.setInt(1, dependentId);
                     ResultSet rs = stmt.executeQuery();
@@ -137,43 +223,22 @@ public class dependentController extends Thread implements Initializable {
                         alert.showAndWait();
                     } else {
                         // handle success
-                        int policyNumber = rs.getInt("policyNumber");
-                        PreparedStatement stmt2 = conn.prepareStatement(sql2);
-                        stmt2.setInt(1, policyNumber);
-                        ResultSet rs2 = stmt2.executeQuery();
-                        if (!rs2.next()){
-                            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                            alert.setTitle("Retrieve Claim notification");
-                            alert.setHeaderText(null);
-                            alert.setContentText("The Claim corresponds to this id cannot be found");
-                            alert.showAndWait();
-                        }
-                        else {
-                            int customerId = rs2.getInt("id");
-                            System.out.println("SQL Query: " + sql3);
-                            System.out.println("Parameter: " + customerId);
-                            PreparedStatement stmt3 = conn.prepareStatement(sql3);
-                            stmt3.setInt(1, customerId);
-                            ResultSet rs3 = stmt3.executeQuery();
-                            if (rs3.next()){
-                                Claim claim = new Claim(
-                                        rs3.getString("id"),
-                                        rs3.getString("Claim_Date"),
-                                        rs3.getString("Exam_Date"),
-                                        rs3.getString("Claim_amount"),
-                                        rs3.getString("Insured_Person"),
-                                        rs3.getString("Status"),
-                                        rs3.getString("Documents"),
-                                        rs3.getString("Receiver_Banking_Infor")
-                                );
-                                claimData.add(claim);
-                                System.out.println("Claim object created: " + claim); // Debug line
-                                System.out.println("Claim object added to ObservableList."); // Debug line
-                                System.out.println("Size of claimData: " + claimData.size()); // Debug line
-                            } else {
-                                // handle case where no rows were returned
-                                System.out.println("No claims found for customer ID: " + customerId);
-                            }
+                        int customerId = rs.getInt("policyNumber");
+                        PreparedStatement stmt3 = conn.prepareStatement(sql3);
+                        stmt3.setInt(1, customerId);
+                        ResultSet rs3 = stmt3.executeQuery();
+                        while (rs3.next()){
+                            Claim claim = new Claim(
+                                    rs3.getString("id"),
+                                    rs3.getString("Claim_Date"),
+                                    rs3.getString("Exam_Date"),
+                                    rs3.getString("Claim_amount"),
+                                    rs3.getString("Insured_Person"),
+                                    rs3.getString("Status"),
+                                    rs3.getString("Documents"),
+                                    rs3.getString("Receiver_Banking_Infor")
+                            );
+                            claimData.add(claim);
                         }
                     }
                 }
@@ -190,6 +255,14 @@ public class dependentController extends Thread implements Initializable {
             e.printStackTrace();
         }
         claimTable.setItems(claimData);
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        claimDateColumn.setCellValueFactory(new PropertyValueFactory<>("Claim_Date"));
+        examDateColumn.setCellValueFactory(new PropertyValueFactory<>("Exam_Date"));
+        claimAmountColumn.setCellValueFactory(new PropertyValueFactory<>("Claim_amount"));
+        insuredPersonColumn.setCellValueFactory(new PropertyValueFactory<>("Insured_Person"));
+        statusColumn.setCellValueFactory(new PropertyValueFactory<>("Status"));
+        documentsColumn.setCellValueFactory(new PropertyValueFactory<>("Documents"));
+        receiverBankingInfoColumn.setCellValueFactory(new PropertyValueFactory<>("Receiver_Banking_Infor"));
     }
 
     public void Logout() throws IOException {
@@ -209,10 +282,6 @@ public class dependentController extends Thread implements Initializable {
             @Override
             public void run() {
                 RetrieveDependent();
-                dependentIdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
-                dependentNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-                dependentEmailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
-                dependentPolicyNumberColumn.setCellValueFactory(new PropertyValueFactory<>("policyNumber"));
             }
         };
 
@@ -221,23 +290,25 @@ public class dependentController extends Thread implements Initializable {
             @Override
             public void run() {
                 RetrieveClaim();
-                idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
-                claimDateColumn.setCellValueFactory(new PropertyValueFactory<>("Claim_Date"));
-                examDateColumn.setCellValueFactory(new PropertyValueFactory<>("Exam_Date"));
-                claimAmountColumn.setCellValueFactory(new PropertyValueFactory<>("Claim_amount"));
-                insuredPersonColumn.setCellValueFactory(new PropertyValueFactory<>("Insured_Person"));
-                statusColumn.setCellValueFactory(new PropertyValueFactory<>("Status"));
-                documentsColumn.setCellValueFactory(new PropertyValueFactory<>("Documents"));
-                receiverBankingInfoColumn.setCellValueFactory(new PropertyValueFactory<>("Receiver_Banking_Infor"));
+            }
+        };
+
+        // Create Runnable for RetrieveHolder
+        Runnable retrieveHolderRunnable = new Runnable() {
+            @Override
+            public void run() {
+                RetrieveHolder();
             }
         };
 
         // Create Thread objects
         Thread retrieveDependentThread = new Thread(retrieveDependentRunnable);
         Thread retrieveClaimThread = new Thread(retrieveClaimRunnable);
+        Thread retrieveHolderThread = new Thread(retrieveHolderRunnable);
 
         // Start the threads
         retrieveDependentThread.start();
         retrieveClaimThread.start();
+        retrieveHolderThread.start();
     }
 }
